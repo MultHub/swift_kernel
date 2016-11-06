8e76619033b654921e121cdd97e57c049b02ea6b
N70 <gameexpertnetwork@gmail.com>
2016-11-05 22:54:01 -0400

    Eh, maybe this will work instead.

--- sbin/tests/00_proc_test.lua
+++ sbin/tests/00_proc_test.lua
@@ -3,7 +3,6 @@ print("this is just debug! hi data! (ba dum tsh)")
 _G.MARCO_ID = proc.create( loadfile("/sbin/tests/marco.lua"), "Marco" )
 _G.POLO_ID = proc.create( loadfile("/sbin/tests/polo.lua"), "Polo" )
 
-local done = false
 
 howlci.log("debug", "Current running processes:")
 local procyon = proc.list() --I was gonna name the var "procl" but then I saw a huge chance to name it "procyon"! Procyon is a star, also know as Alpha Canis Minoris.
@@ -16,12 +15,8 @@ sleep(2)
 if _G.GOTMARCO and _G.GOTPOLO then
   proc.kill( _G.MARCO_ID )
   proc.kill( _G.POLO_ID )
-  done = true
   print("marco/polo success!")
   howlci.log("info", "Marco/Polo success!")
-end
-
-if done then
   howci.log("debug", "exiting proc test")
 else
   howlci.status("fail", "Marco/Polo process test failed!")
